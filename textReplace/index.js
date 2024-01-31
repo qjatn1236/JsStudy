aaa25a671cdce013d804c4f3443d9be8e904fa6a
김범수 <qjatn1236@naver.com>
2024-01-31 23:33:47 +0900

    class 수정

--- textReplace/index.js
+++ textReplace/index.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".findDiv input").value = "우린";
-  document.querySelector(".findDiv textarea").textContent = 
-  `불안한 마음은 언제나 가슴 한켠에 있잖아 내일이 어떤 모양일지 우리는 모르니까 계속 떨고 있는 거야 맘 편히 못 있는 거야 환하게 웃지도 못하고 그저 두려움만 가득하지 검은 파도가 (Never) 덮치게 되면 (No) 그 자리에 서서 두 손을 꼭 쥐겠지만 절대로 잊지 마 밤이 널 삼키려 해도 (Remember) 새벽은 찾아와 (Always) Sometimes we fall and then we rise 늘 반복해 끝도 없이 희망이 떠오르면 절망은 저무니까 기쁨만 기억하고 살자 우린 우린 눈앞이 다 깜깜해도 어둠이 짙어 보여도 틀림없는 사실은 다시 빛은 돌아와 모든 걸 바라보며 살자 우린 우린 우린 노력으로는 안 되는 (Never) 불현듯 찾아오는 아픔 (No) 아무리 미리 막아도 (Never) 아무리 원치 않았어도 언젠가는 찾아오기 마련인 거니까 (Remember) 그냥 그런 거야 (Always) Sometimes we fall and then we rise 늘 반복해 끝도 없이 희망이 떠오르면 절망은 저무니까 기쁨만 기억하고 살자 우린 우린 우린 When the night Dims the sky Don't you forget 무슨 일이 있더라도 새벽은 찾아와 Sometimes we fall and then we rise 늘 반복해 끝도 없이 희망이 떠오르면 절망은 저무니까 기쁨만 기억하고 살자 우린 우린 눈앞이 다 깜깜해도 어둠이 짙어 보여도 틀림없는 사실은 다시 빛은 돌아와 모든 걸 바라보며 살자 우린 우린 우린`;
+  document.querySelector(
+    ".findDiv textarea"
+  ).textContent = `불안한 마음은 언제나 가슴 한켠에 있잖아 내일이 어떤 모양일지 우리는 모르니까 계속 떨고 있는 거야 맘 편히 못 있는 거야 환하게 웃지도 못하고 그저 두려움만 가득하지 검은 파도가 (Never) 덮치게 되면 (No) 그 자리에 서서 두 손을 꼭 쥐겠지만 절대로 잊지 마 밤이 널 삼키려 해도 (Remember) 새벽은 찾아와 (Always) Sometimes we fall and then we rise 늘 반복해 끝도 없이 희망이 떠오르면 절망은 저무니까 기쁨만 기억하고 살자 우린 우린 눈앞이 다 깜깜해도 어둠이 짙어 보여도 틀림없는 사실은 다시 빛은 돌아와 모든 걸 바라보며 살자 우린 우린 우린 노력으로는 안 되는 (Never) 불현듯 찾아오는 아픔 (No) 아무리 미리 막아도 (Never) 아무리 원치 않았어도 언젠가는 찾아오기 마련인 거니까 (Remember) 그냥 그런 거야 (Always) Sometimes we fall and then we rise 늘 반복해 끝도 없이 희망이 떠오르면 절망은 저무니까 기쁨만 기억하고 살자 우린 우린 우린 When the night Dims the sky Don't you forget 무슨 일이 있더라도 새벽은 찾아와 Sometimes we fall and then we rise 늘 반복해 끝도 없이 희망이 떠오르면 절망은 저무니까 기쁨만 기억하고 살자 우린 우린 눈앞이 다 깜깜해도 어둠이 짙어 보여도 틀림없는 사실은 다시 빛은 돌아와 모든 걸 바라보며 살자 우린 우린 우린`;
 
   // 검색하기 버튼 클릭 시
   document.querySelector(".goFindText").addEventListener("click", () => {
@@ -45,8 +46,7 @@ document.addEventListener("DOMContentLoaded", () => {
     for (let i = 0; i < changeNum; i++) {
       let optionElement = document.createElement("option");
       optionElement.value = i;
-      optionElement.textContent =
-        document.querySelector(".findDiv input").value;
+      optionElement.textContent = document.querySelector(".findDiv input").value;
       document.querySelector(".selectList").appendChild(optionElement);
     }
   });
@@ -60,15 +60,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // 조회한 셀렉트 옵션을 돌면서 찾은 셀렉트의 value와 같을 경우 색상을 변경
     for (let i = 0; i < findedList.length; i++) {
-      if (i == selectNum) {
-        findedList[i].style.color = "blue";
-      } else {
-        findedList[i].style.color = "red";
-      }
+      i == selectNum ? (findedList[i].style.color = "blue") : (findedList[i].style.color = "red");
+      // if (i == selectNum) {
+      //   findedList[i].style.color = "blue";
+      // } else {
+      //   findedList[i].style.color = "red";
+      // }
     }
   });
 });
 
-var clsWord = {
-
-}
+var clsWord = {};
