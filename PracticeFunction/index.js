020fc69527b2fd9af96af921ebe674d3aadaa133
김범수 <qjatn1236@naver.com>
2024-02-17 21:52:20 +0900

    함수 과제 추가

--- PracticeFunction/index.js
+++ PracticeFunction/index.js
@@ -3,35 +3,45 @@
   const arrAmount = [5000, 10000, 50000];
 
   arrAmount.forEach((e, i) => {
+    // items에 arrAmount 배열에있는 금액을 담아줄 Element선택
     const items = document.querySelector(`.amount${i + 1}`);
-    items.textContent = e;
+    // 금액에 콤마 추가
+    const itemsDot = e.toLocaleString();
+    // 콤마 추가된 금액 담아주기
+    items.textContent = itemsDot;
   });
 })();
 
 // 금액 추가
-function setAmount(price) {
+function setAmount(event) {
+  // 초기에 셋팅된 금액 담아줄 그릇
+  const price = event.parentElement.firstElementChild.textContent;
+  // 그릇에서 콤마 제거
+  let priceDot = parseFloat(price.replace(/,/g, ""));
+
+  // 현재 토탈금액 담아줄 그릇
   const nowTotalPrice = document.querySelector(".amountResult").textContent;
+
+  // 클릭한 요소가 +인지 -인지 체크
+  const getClickPlusElement = event.classList[0].includes("Plus");
+  const getClickMinusElement = event.classList[0].includes("Minus");
+
   // 콤마 제거
   let nowTotalAmountNumber = parseFloat(nowTotalPrice.replace(/,/g, ""));
-  nowTotalAmountNumber += price;
 
+  if (getClickPlusElement) {
+    nowTotalAmountNumber += priceDot;
+  } else if (getClickMinusElement) {
+    nowTotalAmountNumber -= priceDot;
+  }
+
+  //  금액이 10만원 넘는지 체크
   if (amountLimit(nowTotalAmountNumber)) return;
 
-  // 다시 콤마 추가
-  let result = nowTotalAmountNumber.toLocaleString();
-  document.querySelector(".amountResult").textContent = result;
-}
-
-// 금액 빼기
-function subtractAmount(price) {
-  const nowTotalPrice = document.querySelector(".amountResult").textContent;
-
-  // 콤마 제거
-  let nowTotalAmountNumber = parseFloat(nowTotalPrice.replace(/,/g, ""));
-  nowTotalAmountNumber -= price;
-
+  // 금액 마이너스인지 체크
   if (nowTotalAmountNumber < -1) {
     alert("금액이 마이너스 입니다.");
+    return false;
   }
 
   // 다시 콤마 추가
@@ -41,8 +51,8 @@ function subtractAmount(price) {
 
 // 구매수량 변경
 document.querySelector(".setMaxNumberButton").addEventListener("click", () => {
-  let nMaxNumber = document.querySelector(".inputMaxNumber").value;
-  let setItemsElement = document.querySelectorAll(".setMaxNum");
+  const nMaxNumber = document.querySelector(".inputMaxNumber").value;
+  const setItemsElement = document.querySelectorAll(".setMaxNum");
 
   setItemsElement.forEach(function (i) {
     i.textContent = "+" + nMaxNumber;
@@ -69,6 +79,7 @@ function onlyNumbers(event) {
   event.target.value = event.target.value.replace(/[^0-9]/g, "");
 }
 
+// 금액 초과 체크
 function amountLimit(amount) {
   if (amount > 100000) {
     alert("금액이 100,000을 초과했어요.");
