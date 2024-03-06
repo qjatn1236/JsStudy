bebcde20e9a73e2ddcc1935c3ee8076ede8a4b9a
beonsoo Kim <qjatn1236@gmail.com>
2024-03-06 17:50:09 +0900

    Feat: 페이징 수정해야함

--- paging/app.js
+++ paging/app.js
@@ -1,54 +1,50 @@
 const apiUrl = "https://dummyjson.com/products?limit=5&&skip=0&select=title,price";
 const nowUrl = new URL(apiUrl);
-const pageViewLimit = 5;
+const VIEW_ITEM_COUNT = 5;
 
-fetch("https://dummyjson.com/products?limit=5&&skip=0&select=title,price")
-  .then((res) => res.json())
-  .then((data) => {
-    const itemBox = document.querySelector(".listBox");
+try {
+  fetch(apiUrl)
+    .then(function (res) {
+      if (!res.ok) {
+        throw new Error();
+      }
+      return res.json();
+    })
 
-    data.products.forEach((elements) => {
-      const addDivBox = document.createElement("div");
+    .then((data) => {
+      const itemBox = document.querySelector(".listBox");
 
-      addDivBox.textContent = elements.title;
-      itemBox.appendChild(addDivBox);
-    });
-  });
+      data.products.forEach((elements) => {
+        const addDivBox = document.createElement("div");
 
-const pageNumbers = document.querySelectorAll(".page_num");
-pageNumbers.forEach((pageNumber) => {
-  pageNumber.addEventListener("click", () => {
-    const clickedPage = pageNumber.getAttribute("data-page");
-    const urlParams = nowUrl.searchParams.get("skip");
-
-    const queryParams = { skip: urlParams };
-
-    console.log(queryParams);
-
-    const wantPage = Number(urlParams * clickedPage);
-    // const wantPage = Number(clickedPage * pageViewLimit + urlParams);
-    // console.log(wantPage);
-
-    const itemBox = document.querySelector(".listBox");
-    itemBox.innerHTML = "";
-
-    fetch(`https://dummyjson.com/products?limit=5&&skip=${queryParams.skip}&select=title,price`)
-      // fetch(`https://dummyjson.com/products?limit=5&&skip=5&select=title,price`)
-      .then((res) => res.json())
-      .then((data) => {
-        data.products.forEach((elements) => {
-          const addDivBox = document.createElement("div");
-
-          addDivBox.textContent = elements.title;
-          itemBox.appendChild(addDivBox);
-        });
+        addDivBox.textContent = elements.title;
+        itemBox.appendChild(addDivBox);
       });
+    });
+} catch (error) {
+  console.log(["에러 발생"] + error);
+}
 
-    // const urlParams = nowUrl.searchParams.get("skip");
-    // console.log(parseInt(clickedPage));
-    // console.log(parseInt(urlParams));
+// const pageNumbers = document.querySelectorAll(".page_num");
+// pageNumbers.forEach((pageNumber) => {
+//   pageNumber.addEventListener("click", () => {
+//     const clickedPage = pageNumber.getAttribute("data-page");
+//     const wantPageNum = (clickedPage - 1) * VIEW_ITEM_COUNT;
 
-    // console.log(clickedPage + " ex " + urlParams);
-    // console.log(clickedPage  urlParams);
-  });
-});
+//     nowUrl.searchParams.set("skip", wantPageNum);
+
+//     const itemBox = document.querySelector(".listBox");
+//     itemBox.innerHTML = "";
+
+//     fetch(nowUrl)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         data.products.forEach((elements) => {
+//           const addDivBox = document.createElement("div");
+
+//           addDivBox.textContent = elements.title;
+//           itemBox.appendChild(addDivBox);
+//         });
+//       });
+//   });
+// });
