3b1ae3a57ce106bf359e080d202011a6df233b1a
김범수 <qjatn1236@naver.com>
2024-03-09 10:05:19 +0900

    과제

--- PracticeFunction/index.js
+++ PracticeFunction/index.js
@@ -1,35 +1,30 @@
-// 초기에 금액 셋팅
-let Practice = {
-  init: function () {
-    // 초기화
-  },
-
-  setPriceItems: function (...arg) {
-    arg.forEach((e, i) => {
+fetch("./study.json")
+  .then((res) => res.json())
+  .then((data) => {
+    const moneyItem = Object.keys(data.moneyInformation);
+    moneyItem.forEach((e, i) => {
       document.querySelector(".moneyDivBox").innerHTML += `
-        <div class="moneyDiv">
-          <p class="amount amount${i + 1}"></p>
-          <span>원</span>
-          <button type="button" class="setAmount${i + 1}Plus" onclick="setAmount(this)">+</button>
-          <button type="button" class="setAmount${i + 1}Minus" onclick="setAmount(this)">-</button>
-          <button type="button" class="setAmount${i + 1}MaxNumber setMaxNum" onclick="setMaxAmount(${e})"></button>
-        </div>
-      `;
-
+          <div class="moneyDiv">
+            <p class="amount amount${i + 1}"></p>
+            <span>원</span>
+            <button type="button" class="setAmount${i + 1}Plus" onclick="setAmount(this, ${data.max_money}, ${data.min_money})">+</button>
+            <button type="button" class="setAmount${i + 1}Minus" onclick="setAmount(this, ${data.max_money}, ${data.min_money})">-</button>
+            <button type="button" class="setAmount${i + 1}MaxNumber setMaxNum" onclick="setMaxAmount(${e})"></button>
+          </div>
+        `;
       // items에 arrAmount 배열에있는 금액을 담아줄 Element선택
       const items = document.querySelector(`.amount${i + 1}`);
+
       // 금액에 콤마 추가
-      const itemsDot = e.toLocaleString();
-      console.log(itemsDot);
+      const itemsDot = Number(e).toLocaleString();
 
       // 콤마 추가된 금액 담아주기
       items.textContent = itemsDot;
     });
-  },
-};
+  });
 
 // 금액 추가
-function setAmount(event) {
+function setAmount(event, max, min) {
   // 초기에 셋팅된 금액 담아줄 그릇
   const price = event.parentElement.firstElementChild.textContent;
   // 그릇에서 콤마 제거
@@ -51,13 +46,9 @@ function setAmount(event) {
     nowTotalAmountNumber -= priceDot;
   }
 
-  //  금액이 10만원 넘는지 체크
-  if (amountLimit(nowTotalAmountNumber)) return;
-
-  // 금액 마이너스인지 체크
-  if (nowTotalAmountNumber < -1) {
-    alert("금액이 마이너스 입니다.");
-    return false;
+  if (nowTotalAmountNumber > max || nowTotalAmountNumber < -min) {
+    alert("부적절한 조건입니다.");
+    return;
   }
 
   // 다시 콤마 추가
@@ -84,8 +75,6 @@ function setMaxAmount(price) {
   let nowTotalAmountNumber = parseFloat(nowTotalPrice.replace(/,/g, ""));
   nowTotalAmountNumber += price * maxNumber;
 
-  if (amountLimit(nowTotalAmountNumber)) return;
-
   let result = nowTotalAmountNumber.toLocaleString();
   document.querySelector(".amountResult").textContent = result;
 }
@@ -93,12 +82,4 @@ function setMaxAmount(price) {
 // 인풋 숫자만 입력가능
 function onlyNumbers(event) {
   event.target.value = event.target.value.replace(/[^0-9]/g, "");
-}
-
-// 금액 초과 체크
-function amountLimit(amount) {
-  if (amount > 100000) {
-    alert("금액이 100,000을 초과했어요.");
-    return true;
-  }
 }
