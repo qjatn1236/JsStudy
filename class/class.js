2782cc64bc967ad48c0fa8a86f1fbe7a35d2098a
김범수 <qjatn1236@naver.com>
2024-03-24 09:54:05 +0900

    브라우저 과제

--- class/class.js
+++ class/class.js
@@ -1,106 +1,22 @@
-// const getFetchData = {
-//   isOs: "",
-//   isDataType: "",
-
-//   init: function () {
-//     this.isOs = this.isBrowser();
-
-//     if (this.isDataType === "fetch") {
-//       console.log("fetch");
-//     }
-
-//     console.log("브라우저는 " + this.isOs);
-//     console.log("사용할 데이터 통신은 " + this.isDataType);
-//   },
-
-//   isBrowser: function () {
-//     let agent = window.navigator.userAgent.toLowerCase();
-//     console.log(agent);
-//     let ieName = navigator.appName;
-
-//     if (agent.indexOf("edge") > -1) {
-//       // MS 엣지
-//       this.isDataType = "fetch";
-//       return "edge";
-//     }
-
-//     if (agent.indexOf("edg/") > -1) {
-//       // 크롬기반 엣지
-//       this.isDataType = "fetch";
-//       console.log("크롬기반 엣지");
-//       return "edge";
-//     }
-
-//     if (agent.indexOf("chrome") > -1) {
-//       this.isDataType = "fetch";
-//       return "chrome";
-//     }
-
-//     if (agent.indexOf("safari") > -1) {
-//       this.isDataType = "fetch";
-//       return "safari";
-//     }
-
-//     if (agent.indexOf("firefox") > -1) {
-//       this.isDataType = "fetch";
-//       return "firefox";
-//     }
-
-//     if (ieName === "Microsoft Internet Explorer") {
-//       alert("IE는 지원하지 않습니다.");
-//       return "ie";
-//     }
-//   },
-
-//   runFetch: async function (url) {
-//     try {
-//       const response = await fetch(url);
-//       if (!response.ok) {
-//         throw new Error("Network response was not ok");
-//       }
-
-//       const data = await response.json();
-//       console.log(data);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   },
-
-//   runAjax: async function (url) {
-//     try {
-//       const response = await $.ajax({
-//         url: url,
-//         method: "GET",
-//         dataType: "json",
-//       });
-//       console.log(response);
-//       return response;
-//     } catch (error) {
-//       console.error("There was a problem with the fetch operation:", error);
-//     }
-//   },
-
-//   getData: function (url) {
-//     this.isDataType == "fetch" ? this.runFetch(url) : this.runAjax(url);
-//   },
-// };
-
 class BrowserHandler {
   constructor() {
     this.isOs = "";
     this.isDataType = "";
   }
 
-  getBrowser() {
+  // 초기화
+  getBrowser(apiUrl) {
     this.isOs = this.browserChecking();
 
     console.log(this.isOs);
     console.log(this.isDataType);
+    
+    this.isDataType === "fetch" ? console.log("패치") : console.log("엑시오스")
+    this.isDataType === "fetch" ? this.getFetch(apiUrl) : this.getAxios(apiUrl)
   }
 
   browserChecking() {
     let agent = window.navigator.userAgent.toLowerCase();
-    console.log(agent);
 
     let ieName = navigator.appName;
 
@@ -138,9 +54,9 @@ class BrowserHandler {
     }
   }
 
-  async getFetch() {
+  async getFetch(apiUrl) {
     try {
-      const res = await fetch();
+      const res = await fetch(apiUrl);
       // fetch API는 네트워크 오류가 발생했을때만 프로미스를 거부하는 특징이있다.
       if (!res.ok) {
         console.log(`HTTP Error! status: ${res.status}`);
@@ -149,6 +65,18 @@ class BrowserHandler {
       console.log(data);
     } catch (error) {
       console.error(error.message);
+    }
+  }
+
+  async getAxios(apiUrl){
+    try {
+      const res = await axios.get(apiUrl)
+      if (res.status !== 200) {
+        console.log(`HTTP Error! status: ${res.status}`);
+      }
+      console.log(res.data);
+    } catch (error) {
+      console.error(error);
     }
   }
 }
